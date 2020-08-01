a10926ca685d4874e7d6a537e7209b4e3b5a0d7a
DaeBin HA <dbha@sk.com>
2020-08-02 05:00:36 +0900

    update

--- tasks/credhub-interpolate.sh
+++ tasks/credhub-interpolate.sh
@@ -1,46 +1,48 @@
 #!/usr/bin/env bash
-# code_snippet download-product-script start bash
+# code_snippet credhub_interpolate-script start bash
 
 cat /var/version && echo ""
-set -eux
+set -euo pipefail
 
-if [ -z "$SOURCE" ]; then
-  echo "No source was provided."
-  echo "Please provide pivnet, s3, gcs, or azure."
+
+
+
+# NOTE: The credhub cli does not ignore empty/null environment variables.
+# https://github.com/cloudfoundry-incubator/credhub-cli/issues/68
+if [ -z "$CREDHUB_CA_CERT" ]; then
+  unset CREDHUB_CA_CERT
+  export SKIP_TLS_VALIDATION="--skip-tls-validation" ##!!! added from original version
+fi
+
+
+##!!! modified from original version
+credhub login -s $CREDHUB_SERVER --client-name=$CREDHUB_CLIENT --client-secret=$CREDHUB_SECRET $SKIP_TLS_VALIDATION
+credhub find
+
+
+if [ -z "$PREFIX" ]; then
+  echo "Please specify a PREFIX. It is required."
   exit 1
 fi
 
-vars_files_args=("")
-for vf in ${VARS_FILES}
-do
-  vars_files_args+=("--vars-file ${vf}")
+
+# $INTERPOLATION_PATHS needs to be globbed to read multiple files
+# shellcheck disable=SC2086
+files=$(cd files && find $INTERPOLATION_PATHS -type f -name '*.y*ml' -follow) ##!!! modified from original version :'*.y*ml'
+
+if [ "$SKIP_MISSING" == "true" ]; then
+  export SKIP_MISSING="--skip-missing"
+else
+  export SKIP_MISSING=""
+fi
+
+
+for file in $files; do
+  echo "interpolating files/$file"
+  mkdir -p interpolated-files/"$(dirname "$file")"
+  credhub interpolate --prefix "$PREFIX" \
+  --file files/"$file" ${SKIP_MISSING} \
+  > interpolated-files/"$file"
 done
 
-# ${vars_files_args[@] needs to be globbed to pass through properly
-# shellcheck disable=SC2068
-om download-product \
-   --config config/"${CONFIG_FILE}" ${vars_files_args[@]} \
-   --output-directory downloaded-files \
-   --source "$SOURCE"
-
-{ printf "\nReading product details..."; } 2> /dev/null
-# shellcheck disable=SC2068
-product_slug=$(om interpolate \
-  --config config/"${CONFIG_FILE}" ${vars_files_args[@]} \
-  --path /pivnet-product-slug)
-
-product_path=$(om interpolate \
-  --config downloaded-files/download-file.json \
-  --path /product_path)
-
-stemcell_file=$(om interpolate \
-  --config downloaded-files/download-file.json \
-  --path /stemcell_path?)
-
-echo $product_slug > downloaded-product/product_slug
-echo $product_path > downloaded-product/product_path
-echo $stemcell_file > downloaded-product/stemcell_file
-
-echo $product_path  | awk -F '[/\[\]]' '{print $NF}' > downloaded-product/product_file
-
-# code_snippet download-product-script end
+# code_snippet credhub_interpolate-script end
