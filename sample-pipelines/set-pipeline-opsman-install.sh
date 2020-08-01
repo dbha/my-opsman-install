8fcc6b8e343c21bf8ec289a2612f93ae155965da
DaeBin HA <dbha@sk.com>
2020-08-01 15:51:18 +0900

    update

--- sample-pipelines/set-pipeline-opsman-install.sh
+++ sample-pipelines/set-pipeline-opsman-install.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
 
 FOUNDATION=$1
+PIPELINE_NAME=$2
+PIPELINE_YAML="install-opsman.yml"
+WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ -z ${FOUNDATION} ]; then
-  echo "set-pipeline-opsman-install.sh FOUNDATION"
+if [ -z ${FOUNDATION} ] || [ -z ${PIPELINE_NAME} ]  ; then
+  echo "set-pipeline-opsman-install.sh FOUNDATION PIPELINE_NAME"
   exit 0;
 fi
 
-fly -t cp sp -p $FOUNDATION -c ./pipeline.yml -l ./vars.yml
+fly -t cp sp -p "${PIPELINE_NAME}" \
+-c $WORK_DIR/${PIPELINE_YAML} \
+-v foundation=${FOUNDATION} \
+-v pipeline_name="${PIPELINE_NAME}" \
+-l $WORK_DIR/foundations/${FOUNDATION}/pipeline-vars/params.yml
