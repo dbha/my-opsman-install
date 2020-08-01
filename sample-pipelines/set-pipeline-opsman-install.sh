81d5e82040633b75526119fe5139c029a5b44e10
dbha <dbha@example.com>
2020-08-01 08:04:28 +0000

    update

--- sample-pipelines/set-pipeline-opsman-install.sh
+++ sample-pipelines/set-pipeline-opsman-install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 FOUNDATION=$1
-PIPELINE_NAME=$2
+PRODUCT_NAME=$2
 PIPELINE_YAML="install-opsman.yml"
 WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ -z ${FOUNDATION} ] || [ -z ${PIPELINE_NAME} ]  ; then
-  echo "set-pipeline-opsman-install.sh FOUNDATION PIPELINE_NAME"
+if [ -z ${FOUNDATION} ] || [ -z ${PRODUCT_NAME} ]  ; then
+  echo "set-pipeline-opsman-install.sh FOUNDATION PRODUCT_NAME"
   exit 0;
 fi
 
-fly -t cp sp -p "${PIPELINE_NAME}" \
+fly -t cp sp -p "${PRODUCT_NAME-install-opsman" \
 -c $WORK_DIR/${PIPELINE_YAML} \
 -v foundation=${FOUNDATION} \
--v pipeline_name="${PIPELINE_NAME}" \
+-v product-name=${PRODUCT_NAME}
 -l $WORK_DIR/foundations/${FOUNDATION}/pipeline-vars/params.yml
