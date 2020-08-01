39e43a25495a5f48d2a78fb3643a501fe2ac977f
DaeBin HA <dbha@sk.com>
2020-08-01 17:40:44 +0900

    update

---
+++ download-products.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z $1 ]  ; then
+    echo "!!! please provide parameters"
+	echo "${BASH_SOURCE[0]} [foundation]"
+	exit
+fi
+FLY_TARGET=sp
+FOUNDATION=$1
+
+fly -t ${FLY_TARGET} sp -p "${FOUNDATION}-download-products" \
+-c ./download-products.yml \
+-v foundation=${FOUNDATION} \
+-l ./foundations/${FOUNDATION}/pipeline-vars/params.yml
