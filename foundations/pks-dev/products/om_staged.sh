82470207adb3fe834aaeac4aa7b38f036fb6ad1a
DaeBin HA <dbha@sk.com>
2020-08-11 15:27:17 +0900

    update

--- foundations/pks-dev/products/om_staged.sh
+++ foundations/pks-dev/products/om_staged.sh
@@ -1,3 +1,4 @@
 
 #om -e env.yml staged-products
-#om -e env.yml staged-config --include-credentials --product-name pivotal-container-service > pks.yml
+#om -e env.yml staged-config --include-credentials --include-placeholders --product-name pivotal-container-service > pks.yml
+#om -e env.yml staged-config --include-credentials --include-placeholders --product-name p-healthwatch2 > healthwatch2.yml
